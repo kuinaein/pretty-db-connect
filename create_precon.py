2b495c8cad934c0c97c1115c5ca6502a955a9beb
Kuina <kuinanein@gmail.com>
2018-08-16 03:33:24 +0900

    インターネットゲートウェイの設定まで終わり

--- create_precon.py
+++ create_precon.py
@@ -20,6 +20,10 @@ class PrettyDBConnect:
         self.name_tags = [{'Key': 'Name', 'Value': self.name_base}]
 
 
+def boto_first(collection):
+    return list(collection.limit(count=1))[0]
+
+
 def ensure_vpc(pretty: PrettyDBConnect):
     vpc_res = pretty.ec2_client.describe_vpcs(
         Filters=[{'Name': 'tag:Name', 'Values': [pretty.name_base]}])
@@ -36,13 +40,49 @@ def ensure_vpc(pretty: PrettyDBConnect):
         sleep(1)
         vpc.load()
     vpc.create_tags(Tags=pretty.name_tags)
-    security_group = list(vpc.security_groups.limit(count=1))[0]
-    security_group.create_tags(Tags=pretty.name_tags)
-    list(vpc.network_acls.limit(count=1))[0].create_tags(Tags=pretty.name_tags)
-    list(vpc.route_tables.limit(count=1))[0].create_tags(Tags=pretty.name_tags)
+    boto_first(vpc.security_groups).create_tags(Tags=pretty.name_tags)
+    boto_first(vpc.network_acls).create_tags(Tags=pretty.name_tags)
+    boto_first(vpc.route_tables).create_tags(Tags=pretty.name_tags)
     return vpc.id
+
+
+def ensure_subnet(pretty: PrettyDBConnect, vpc_id: str):
+    vpc = pretty.ec2.Vpc(vpc_id)
+    if (0 < len(list(vpc.subnets.limit(count=1)))):
+        return
+
+    logger.info('サブネットの作成中...')
+    subnet = vpc.create_subnet(CidrBlock=PRETTY_CONFIG['VPC_CIDR_BLOCK'])
+    while 'available' != subnet.state:
+        sleep(1)
+        subnet.load()
+    subnet.create_tags(Tags=pretty.name_tags)
+    boto_first(vpc.route_tables).associate_with_subnet(SubnetId=subnet.id)
+    return subnet.id
+
+
+def ensure_internet_gateway(pretty: PrettyDBConnect, vpc_id: str):
+    vpc = pretty.ec2.Vpc(vpc_id)
+    if (0 < len(list(vpc.internet_gateways.limit(count=1)))):
+        return
+
+    logger.info('インターネットゲートウェイの作成中...')
+    gateway_res = pretty.ec2_client.create_internet_gateway()
+    gateway = pretty.ec2.InternetGateway(
+        gateway_res['InternetGateway']['InternetGatewayId'])
+    gateway.create_tags(Tags=pretty.name_tags)
+    vpc.attach_internet_gateway(InternetGatewayId=gateway.id)
+    gateway.load()
+    while 'available' != gateway.attachments[0]['State']:
+        sleep(1)
+        gateway.load()
+    boto_first(vpc.route_tables).create_route(
+        DestinationCidrBlock='0.0.0.0/0', GatewayId=gateway.id)
+    return gateway.id
 
 
 if '__main__' == __name__:
     pretty = PrettyDBConnect()
-    ensure_vpc(pretty)
+    vpc_id = ensure_vpc(pretty)
+    ensure_subnet(pretty, vpc_id)
+    ensure_internet_gateway(pretty, vpc_id)
